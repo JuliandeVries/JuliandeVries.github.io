d4fad8effdaa153ab5cf92ea1c4ca8199d030083
JuliandeVries <Julian.devries@student.rocva.nl>
2021-03-24 11:14:56 +0100

    d

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var leeftijd = prompt("hoe oud ben je");
 var element = document.getElementById("id02");
  
 if(leeftijd > 18){
-    document.getElementById("#kopje1").style.color = 'white';
+    document.getElementById("kopje1").style.color = 'white';
     document.querySelector("body").style.backgroundColor = 'red';
 }
 else{
